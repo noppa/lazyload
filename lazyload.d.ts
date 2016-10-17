3510044692a02ff6c359c53cc8a9a2977314a9bc
Oskari Noppa <the.nobba@gmail.com>
2016-10-17 05:00:24 +0300

    Fix the ts type defs

--- lazyload.d.ts
+++ lazyload.d.ts
@@ -1,7 +1,11 @@
 
-type LoadFunction = <C, T>(path: string, cb: (this: C, arg: T) => void, arg?: T, context?: C) => void;
+interface __LoadFunction {
+  (path: string, cb: () => void): void;
+  <T>(path: string, cb: (arg: T) => void, arg: T): void;
+  <T, C>(path: string, cb: (this: C, arg: T) => void, arg: T, context: C): void;
+}
 
 declare var LazyLoad: {
-  js: LoadFunction,
-  css: LoadFunction
+  js: __LoadFunction,
+  css: __LoadFunction
 };
